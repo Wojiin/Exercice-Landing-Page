685a8fc10778b6eaa21a2c73131c432c8bac4a00
Quentin Maia <q.maia@laposte.net>
2025-09-16 15:56:02 +0200

    ajustement page principale

--- Exercice-Landing page/main.js
+++ Exercice-Landing page/main.js
@@ -1,14 +1,14 @@
 function myFunction() {
-    let myTopNav = document.getElementById("myTopnav");
-    if (myTopNav.className === "topnav") {
-      myTopNav.className += " responsive";
-    } else {
-      myTopNav.className = "topnav";
-    }
-    let burger = document.getElementsByClassName("icon");
-    if(burger.className === "icon"){
-        burger.className+= "burger";
-    } else{
-        burger.className = "icon"
-    }
+  let myTopNav = document.getElementById("myTopnav");
+  if (myTopNav.className === "topnav") {
+    myTopNav.className += " responsive";
+  } else {
+    myTopNav.className = "topnav";
   }
+  let burger = document.getElementsByClassName("icon");
+  if (burger.className === "icon") {
+    burger.className += "burger";
+  } else {
+    burger.className = "icon";
+  }
+}
